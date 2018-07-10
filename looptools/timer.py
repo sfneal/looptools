315f6111a69c99da3574c0804faa0f71b83b3c2d
mrstephenneal <stephen.neal14@gmail.com>
2018-07-10 13:21:24 -0400

    Methods added to Timer class

--- looptools/timer.py
+++ looptools/timer.py
@@ -4,13 +4,21 @@ import time
 class Timer:
     def __init__(self):
         self.start = time.time()
+        self.elapsed_str = None
+        self.elapsed_float = None
+
+    def __str__(self):
+        return str(self.elapsed_str)
+
+    def __float__(self):
+        return float(self.elapsed_float)
 
     @property
     def end(self):
         end = time.time()
-        elapsed = end - self.start
-        if elapsed < 60:
-            elapsed = str('sec: ' + str(elapsed))
+        self.elapsed_float = end - self.start
+        if self.elapsed_float < 60:
+            self.elapsed_str = str('sec: ' + str(self.elapsed_float))
         else:
-            elapsed = str('min: ' + str(elapsed/60))
-        return elapsed
+            self.elapsed_str = str('min: ' + str(self.elapsed_float/60))
+        return self.elapsed_str

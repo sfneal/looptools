e3f77cb455abea61279a3fcbe4323627a5e1b621
mrstephenneal <stephen.neal14@gmail.com>
2018-07-27 09:43:55 -0400

    Updated version for PyPi distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='looptools',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',

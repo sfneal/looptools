49546f7e9a8fa7e15e02f18a38663077ef2c7a6a
mrstephenneal <stephen.neal14@gmail.com>
2018-09-05 10:35:37 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='looptools',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',
     license='MIT Licence',
     author='Stephen Neal',
     author_email='stephen@stephenneal.net',
-    description='Added timer functions for timer decorations and active timers.',
+    description='Lightweight Python help utility.',
     long_description='Helper utility functions for logging output, timing processes and counting iterations.'
 )

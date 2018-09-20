13fc1f245493cf9c12a5fe3631139640eb42262c
mrstephenneal <stephen.neal14@gmail.com>
2018-09-20 09:31:31 -0400

    Updated to version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='looptools',
-    version='0.1.10',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',

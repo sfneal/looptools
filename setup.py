5e29b724f3a5aa79d9f84dd93d9c4164fc48cb12
mrstephenneal <stephen.neal14@gmail.com>
2018-10-24 15:17:34 -0400

    Updated to PyPi version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='looptools',
-    version='1.0.3',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',

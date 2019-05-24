3a5a269f3725da344edaabddd963901d36cd3eba
mrstephenneal <stephen.neal14@gmail.com>
2019-05-24 10:56:19 -0400

    BUMP version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='looptools',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',

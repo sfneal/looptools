19ee43aefb812d9ea278701dd251041a946ac125
mrstephenneal <stephen.neal14@gmail.com>
2019-06-03 10:23:24 -0400

    ADD _version.py file and get_version function to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
+import os
+import re
 from setuptools import setup, find_packages
+
+
+def get_version(source=os.path.dirname(__file__)):
+    """
+    Retrieve the version of a python distribution.
+
+    version_file default is the <project_root>/_version.py
+
+    :param source: Path to project root
+    :return: Version string
+    """
+    version_str_lines = open(os.path.join(source, os.path.basename(source), '_version.py'), "rt").read()
+    version_str_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(version_str_regex, version_str_lines, re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in %s." % os.path.join(source, os.path.basename(source)))
+
 
 setup(
     name='looptools',
-    version='1.2.2',
+    version=get_version(),
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/mrstephenneal/looptools',
